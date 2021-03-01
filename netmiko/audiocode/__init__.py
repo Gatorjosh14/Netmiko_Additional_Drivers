154f71d2a0418bbef2c2745a2d7bae86c66bf074
Joshua Robinson <51381009+Gatorjosh14@users.noreply.github.com>
2021-03-01 11:37:37 -0500

    Delete netmiko/audiocode directory

--- netmiko/audiocode/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from netmiko.audiocode.audiocode_ssh import AudiocodeSSH, AudiocodeTelnet, AudiocodeOldCLI
-
-__all__ = [
-	'AudiocodeSSH',
-	'AudiocodeTelnet'
-	'AudiocodeOldCLI'
-	]
-	
