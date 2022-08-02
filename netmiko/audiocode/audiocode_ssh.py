b92ed6dcac0c64698748f01f7e5cfad51e35ff8b
Joshua Robinson <51381009+Gatorjosh14@users.noreply.github.com>
2022-08-02 15:46:06 -0400

    Update audiocode_ssh.py

--- netmiko/audiocode/audiocode_ssh.py
+++ netmiko/audiocode/audiocode_ssh.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from click import command
 from netmiko.base_connection import BaseConnection
 from netmiko import log
 from netmiko.exceptions import (
@@ -12,6 +13,10 @@ import re
 
 class AudiocodeBaseSSH (BaseConnection):
 	"""Common Methods for AudioCodes running 7.2 CLI for SSH."""
+	def __init__(self, *args, **kwargs) -> None:
+		default_enter = kwargs.get("default_enter")
+		kwargs["default_enter"] = "\r" if default_enter is None else default_enter
+		super().__init__(*args, **kwargs)
 
 	def session_preparation(self):
 		"""Prepare the session after the connection has been established."""
@@ -26,7 +31,7 @@ class AudiocodeBaseSSH (BaseConnection):
 		pri_prompt_terminator="#", 
 		alt_prompt_terminator=">", 
 		delay_factor=1.0,
-		pattern = "(\*?#|\*?>)$"
+		pattern = r"\*?(#|>)"
 		):
 		"""Sets self.base_prompt
 
@@ -47,69 +52,34 @@ class AudiocodeBaseSSH (BaseConnection):
 		:param pattern: Regular expression pattern to search for in find_prompt() call
 		"""
 		
-		return super(AudiocodeBaseSSH, self).set_base_prompt(
+		return super().set_base_prompt(
 			pri_prompt_terminator=pri_prompt_terminator, 
 			alt_prompt_terminator=alt_prompt_terminator,
 			delay_factor=delay_factor,
 			pattern = pattern
 		)
 
-	def find_prompt(self, 
-		delay_factor = 1.0, 
-		max_loops = 10, 
-		pattern = None):
+	def find_prompt(
+		self, 
+		delay_factor: float = 1.0, 
+		pattern: str = r"\*?(#|>)", 
+		) -> str:
 		"""Finds the current network device prompt, last line only.
 
 		:param delay_factor: See __init__: global_delay_factor
 		:type delay_factor: int
 
 		:param pattern: Regular expression pattern to determine whether prompt is valid
+
+		:param confirm_pattern: Regular expression pattern to confirm prompt was found by auto discovery.
 		"""
-		delay_factor = self.select_delay_factor(delay_factor)
-		self.clear_buffer()
-		self.write_channel(self.RETURN)
-		sleep_time = delay_factor * 0.1
-		time.sleep(sleep_time)
+		return super().find_prompt(
+			delay_factor=delay_factor, 
+			pattern=pattern,
+		)
 
-		# Created parent loop to counter wrong prompts due to spamming alarm logs into terminal.
-		max_loops = max_loops
-		loops = 0
-		while loops <= max_loops:
-			# Initial attempt to get prompt
-			prompt = self.read_channel().strip()
-			count = 0
-			while count <= 12 and not prompt:
-				prompt = self.read_channel().strip()
-				if not prompt:
-					self.write_channel(self.RETURN)
-					time.sleep(sleep_time)
-					if sleep_time <= 3:
-						# Double the sleep_time when it is small
-						sleep_time *= 2
-					else:
-						sleep_time += 1
-				count += 1
-
-			# If multiple lines in the output take the last line
-			prompt = prompt.split(self.RESPONSE_RETURN)[-1]
-			prompt = prompt.strip()
-			self.clear_buffer()
-
-			# This verifies a valid prompt has been found before proceeding.
-			if pattern:
-				if re.search(pattern, prompt):
-					break
-			elif not prompt and loops == 9:
-				raise ValueError(f"Unable to find prompt: {prompt}")
-			
-			self.write_channel(self.RETURN)
-			loops += 1
-			time.sleep(1)
-
-		log.debug(f"[find_prompt()]: prompt is {prompt}")
-		return prompt
-
-	def check_config_mode(self, 
+	def check_config_mode(
+		self, 
 		check_string: str = r"(\)#|\)\*#)", 
 		pattern: str = "#", 
 		force_regex: bool = True
@@ -122,7 +92,7 @@ class AudiocodeBaseSSH (BaseConnection):
 		:param pattern: Pattern to terminate reading of channel
 		:type pattern: str
 		"""
-		return super(AudiocodeBaseSSH, self).check_config_mode(
+		return super().check_config_mode(
 			check_string=check_string, pattern=pattern, force_regex=force_regex
 		)
 
@@ -132,7 +102,7 @@ class AudiocodeBaseSSH (BaseConnection):
 		:param check_string: Identification of privilege mode from device
 		:type check_string: str
 		"""
-		return super(AudiocodeBaseSSH, self).check_enable_mode(
+		return super().check_enable_mode(
 			check_string=check_string
 		)
 
@@ -146,7 +116,8 @@ class AudiocodeBaseSSH (BaseConnection):
 		self._session_log_fin = True
 		self.write_channel("exit" + self.RETURN)
 
-	def enable(self,
+	def enable(
+		self,
 		cmd: str = "enable",
 		pattern: str = "ssword",
 		enable_pattern: str = "#",
@@ -162,7 +133,7 @@ class AudiocodeBaseSSH (BaseConnection):
 
 		:param re_flags: Regular expression flags used in conjunction with pattern
 		"""
-		return super(AudiocodeBaseSSH, self).enable(
+		return super().enable(
 			cmd=cmd, pattern=pattern, enable_pattern=enable_pattern, re_flags=re_flags
 		)
 
@@ -175,7 +146,7 @@ class AudiocodeBaseSSH (BaseConnection):
 		:param pattern: Pattern to terminate reading of channel
 		:type pattern: str
 		"""
-		return super(AudiocodeBaseSSH, self).exit_config_mode(
+		return super().exit_config_mode(
 			exit_config=exit_config, pattern=pattern
 		)
 
@@ -185,10 +156,10 @@ class AudiocodeBaseSSH (BaseConnection):
 		:param exit_command: Command that exits the session from privileged mode
 		:type exit_command: str
 		"""
-		return super(AudiocodeBaseSSH, self).exit_enable_mode(
+		return super().exit_enable_mode(
 			exit_command=exit_command
 		)
-			
+
 	def send_config_set(
 		self,
 		config_commands = None,
@@ -199,7 +170,7 @@ class AudiocodeBaseSSH (BaseConnection):
 		strip_prompt: bool = False,
 		strip_command: bool = False,
 		config_mode_command: str = None,
-		cmd_verify: bool = False,
+		cmd_verify: bool = True,
 		enter_config_mode: bool = False,
 		error_pattern: str = "",
 		terminator: str = r"\*?#",
@@ -242,108 +213,21 @@ class AudiocodeBaseSSH (BaseConnection):
 		:param bypass_commands: Regular expression pattern indicating configuration commands
 		where cmd_verify is automatically disabled.
 		"""
-		if self.global_cmd_verify is not None:
-			cmd_verify = self.global_cmd_verify
-
-		if delay_factor is not None or max_loops is not None:
-			#warnings.warn(DELAY_FACTOR_DEPR_SIMPLE_MSG, DeprecationWarning)
-
-			# Calculate an equivalent read_timeout (if using old settings)
-			# Eliminate in Netmiko 5.x
-			if read_timeout is None:
-				max_loops = 150 if max_loops is None else max_loops
-				delay_factor = 1.0 if delay_factor is None else delay_factor
-
-				# If delay_factor has been set, then look at global_delay_factor
-				delay_factor = self.select_delay_factor(delay_factor)
-
-				#read_timeout = calc_old_timeout(
-				#	max_loops=max_loops, delay_factor=delay_factor, loop_delay=0.1
-				#)
-
-		if delay_factor is None:
-			delay_factor = self.select_delay_factor(0)
-		else:
-			delay_factor = self.select_delay_factor(delay_factor)
-
-		if read_timeout is None:
-			read_timeout = 15
-		else:
-			read_timeout = read_timeout
-
-		if config_commands is None:
-			return ""
-		elif isinstance(config_commands, str):
-			config_commands = (config_commands,)
-
-		if not hasattr(config_commands, "__iter__"):
-			raise ValueError("Invalid argument passed into send_config_set")
-
-		if bypass_commands is None:
-			# Commands where cmd_verify is automatically disabled reg-ex logical-or
-			bypass_commands = r"^banner .*$"
-
-		# Set bypass_commands="" to force no-bypass (usually for testing)
-		bypass_detected = False
-		if bypass_commands:
-			bypass_detected = any(
-				[True for cmd in config_commands if re.search(bypass_commands, cmd)]
-			)
-		if bypass_detected:
-			cmd_verify = False
-
-		# Send config commands
-		output = ""
-		if enter_config_mode:
-			if config_mode_command:
-				output += self.config_mode(config_mode_command)
-			else:
-				output += self.config_mode()
-
-		# Perform output gathering line-by-line (legacy way)
-		if self.fast_cli and self._legacy_mode and not error_pattern:
-			for cmd in config_commands:
-				self.write_channel(self.normalize_cmd(cmd))
-			# Gather output
-			output += self.read_channel_timing(read_timeout=read_timeout)
-
-		elif not cmd_verify:
-			for cmd in config_commands:
-				self.write_channel(self.normalize_cmd(cmd))
-				time.sleep(delay_factor * 0.05)
-
-				# Gather the output incrementally due to error_pattern requirements
-				if error_pattern:
-					output += self.read_channel_timing(read_timeout=read_timeout)
-					if re.search(error_pattern, output, flags=re.M):
-						msg = f"Invalid input detected at command: {cmd}"
-						raise ConfigInvalidException(msg)
-
-			# Standard output gathering (no error_pattern) - modified because read_timeout was causing it to fail.
-			if not error_pattern:
-				output += self.read_channel_timing(delay_factor=delay_factor,max_loops=max_loops)
-
-		else:
-			for cmd in config_commands:
-				self.write_channel(self.normalize_cmd(cmd))
-
-				# Make sure command is echoed
-				output += self.read_until_pattern(pattern=re.escape(cmd.strip()))
-
-				# Read until next prompt or terminator (#); the .*$ forces read of entire line
-				pattern = f"(?:{re.escape(self.base_prompt)}.*$|{terminator}.*$)"
-				output += self.read_until_pattern(pattern=pattern, re_flags=re.M)
-
-				if error_pattern:
-					if re.search(error_pattern, output, flags=re.M):
-						msg = f"Invalid input detected at command: {cmd}"
-						raise ConfigInvalidException(msg)
-
-		if exit_config_mode:
-			output += self.exit_config_mode()
-		output = self._sanitize_output(output)
-		log.debug(f"{output}")
-		return output
+		return super().send_config_set(
+			config_commands=config_commands,
+			exit_config_mode=exit_config_mode,
+			read_timeout=read_timeout,
+			delay_factor=delay_factor,
+			max_loops=max_loops,
+			strip_prompt=strip_prompt,
+			strip_command=strip_command,
+			config_mode_command=config_mode_command,
+			cmd_verify=cmd_verify,
+			enter_config_mode=enter_config_mode,
+			error_pattern=error_pattern,
+			terminator=terminator,
+			bypass_commands=bypass_commands
+		)
 
 	def disable_paging(
 		self, 
@@ -366,7 +250,14 @@ class AudiocodeBaseSSH (BaseConnection):
 		self.clear_buffer()
 		disable_window_config = disable_window_config
 		log.debug("In disable_paging")
-		self.send_config_set(disable_window_config,True,.25,150,False,False,None,False,False)
+		self.send_config_set(
+			config_commands = disable_window_config,
+			exit_config_mode = True,
+			read_timeout = None,
+			delay_factor = 1.0,
+			max_loops = 150,
+			enter_config_mode = False,
+		)
 		log.debug("Exiting disable_paging")
 
 	def _enable_paging(
@@ -389,10 +280,17 @@ class AudiocodeBaseSSH (BaseConnection):
 		self.clear_buffer()
 		enable_window_config = enable_window_config
 		log.debug("In _enable_paging")
-		self.send_config_set(enable_window_config,True,.25,150,False,False,None,False,False)
+		self.send_config_set(
+			config_commands = enable_window_config,
+			exit_config_mode = True,
+			read_timeout = None,
+			delay_factor = 1.0,
+			max_loops = 150,
+			enter_config_mode = False,
+		)
 		log.debug("Exiting _enable_paging")
 
-	def save_config(self, cmd="write", confirm=False, confirm_response="done"):
+	def save_config(self, cmd="write", confirm=False, confirm_response=""):
 		"""Saves the running configuration.
 		
 		:param cmd: Command to save configuration
@@ -490,7 +388,7 @@ class Audiocode66SSH(AudiocodeBaseSSH):
 		:type delay_factor: int
 		
 		"""		
-		return super(Audiocode66SSH, self).disable_paging(
+		return super().disable_paging(
 			disable_window_config=disable_window_config, delay_factor=delay_factor
 		)
 		
@@ -508,7 +406,7 @@ class Audiocode66SSH(AudiocodeBaseSSH):
 		:type delay_factor: int
 		
 		"""
-		return super(Audiocode66SSH, self)._enable_paging(
+		return super()._enable_paging(
 			enable_window_config=enable_window_config, delay_factor=delay_factor
 		)
 
@@ -524,17 +422,18 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		"""Prepare the session after the connection has been established."""
 		self.write_channel(self.RETURN)
 		self.write_channel(self.RETURN)
-		self._test_channel_read(pattern="/>")
+		self._test_channel_read(pattern=r"/>")
 		self.set_base_prompt()
 		# Clear the read buffer
 		time.sleep(0.3 * self.global_delay_factor)
 		self.clear_buffer()
 
-	def set_base_prompt(self, 
-		pri_prompt_terminator="/>", 
+	def set_base_prompt(
+		self, 
+		pri_prompt_terminator=r"/>", 
 		alt_prompt_terminator="", 
 		delay_factor=1.0,
-		pattern = "/>"
+		pattern = r"/>"
 		):
 		"""Sets self.base_prompt
 
@@ -563,16 +462,41 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 			self.base_prompt = prompt
 			return self.base_prompt
 
+	def find_prompt(
+		self, 
+		delay_factor: float = 1.0, 
+		pattern: str = r"/>", 
+		) -> str:
+		"""Finds the current network device prompt, last line only.
+
+		:param delay_factor: See __init__: global_delay_factor
+		:type delay_factor: int
+
+		:param pattern: Regular expression pattern to determine whether prompt is valid
+
+		:param confirm_pattern: Regular expression pattern to confirm prompt was found by auto discovery.
+		"""
+		return super().find_prompt(
+			delay_factor=delay_factor, 
+			pattern=pattern,
+		)
+
 	def enable(self, cmd="", pattern="", re_flags=re.IGNORECASE):
-		"""Not in use"""
+		"""Not supported"""
 		pass
 
-	def check_enable_mode(self, check_string="#"):
-		"""Not in use"""
-		pass
+	def check_enable_mode(self, check_string=r"/>"):
+		"""Check if in enable mode. Return boolean.
+
+		:param check_string: Identification of privilege mode from device
+		:type check_string: str
+		"""
+		return super().check_enable_mode(
+			check_string=check_string
+		)
 
 	def exit_enable_mode(self, exit_command=""):
-		"""Not in use"""
+		"""Not supported"""
 		pass
 
 	def send_config_set(
@@ -585,10 +509,10 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		strip_prompt: bool = False,
 		strip_command: bool = False,
 		config_mode_command: str = None,
-		cmd_verify: bool = False,
+		cmd_verify: bool = True,
 		enter_config_mode: bool = False,
 		error_pattern: str = "",
-		terminator: str = r"/>",
+		terminator: str = r"/.*>",
 		bypass_commands: str = None,
 		) -> str:
 		"""
@@ -628,7 +552,7 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		:param bypass_commands: Regular expression pattern indicating configuration commands
 		where cmd_verify is automatically disabled.
 		"""
-		return super(AudiocodeShellSSH, self).send_config_set(
+		return super().send_config_set(
 			config_commands=config_commands,
 			exit_config_mode=exit_config_mode,
 			read_timeout=read_timeout,
@@ -644,10 +568,29 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 			bypass_commands=bypass_commands
 		)
 
-	def check_config_mode(self, 
-		check_string: str = "/CONFiguration", 
-		pattern: str = "", 
-		force_regex: bool = False
+	def config_mode(
+		self, config_command: str = "", pattern: str = r"/.*>", re_flags: int = 0
+		) -> str:
+		"""Enter into config_mode.
+
+		:param config_command: Configuration command to send to the device
+		:type config_command: str
+
+		:param pattern: Pattern to terminate reading of channel
+		:type pattern: str
+
+		:param re_flags: Regular expression flags
+		:type re_flags: RegexFlag
+		"""
+		return super().config_mode(
+			config_command=config_command, pattern=pattern, re_flags=re_flags
+		)
+
+	def check_config_mode(
+		self, 
+		check_string: str = r"/CONFiguration>", 
+		pattern: str = r"/.*>", 
+		force_regex: bool = True
 		) -> bool:
 		"""Checks if the device is in configuration mode or not.
 
@@ -657,11 +600,11 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		:param pattern: Pattern to terminate reading of channel
 		:type pattern: str
 		"""
-		return super(AudiocodeShellSSH, self).check_config_mode(
+		return super().check_config_mode(
 			check_string=check_string, pattern=pattern, force_regex=force_regex
 		)
 
-	def exit_config_mode(self, exit_config="..", pattern="/>"):
+	def exit_config_mode(self, exit_config="..", pattern=r"/>"):
 		"""Exit from configuration mode.
 
 		:param exit_config: Command to exit configuration mode
@@ -670,18 +613,19 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		:param pattern: Pattern to terminate reading of channel
 		:type pattern: str
 		"""
-		return super(AudiocodeShellSSH, self).exit_config_mode(
+		return super().exit_config_mode(
 			exit_config=exit_config, pattern=pattern
 		)
 
 	def disable_paging(self):
-		"""Not in use"""
+		"""Not supported"""
 		pass
 
-	def _save_config(self, 
+	def save_config(
+		self, 
 		cmd="SaveConfiguration", 
-		confirm=False, 
-		confirm_response="Configuration has been saved"
+		confirm=False,
+		confirm_response=""
 		):
 		"""Saves the running configuration.
 		
@@ -695,11 +639,12 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		:type confirm response: str
 		
 		"""
-		return super(AudiocodeShellSSH, self)._save_config(
+		return super().save_config(
 			cmd=cmd, confirm=confirm, confirm_response=confirm_response
 		)
 		
-	def _reload_device(self, 
+	def _reload_device(
+		self, 
 		reload_device=True, 
 		reload_save=True, 
 		cmd_save="SaveAndReset", 
@@ -724,7 +669,7 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 		:type reload_message: str
 
 		"""
-		return super(AudiocodeShellSSH, self)._reload_device(
+		return super()._reload_device(
 			reload_device=reload_device,
 			reload_save=reload_save,
 			cmd_save=cmd_save,
@@ -735,13 +680,50 @@ class AudiocodeShellSSH(AudiocodeBaseSSH):
 	def _device_terminal_exit(self, command="exit"):
 		"""This is for accessing devices via terminal. It first reenables window paging for
 		future use and exits the device before you send the disconnect method"""
-		return super(AudiocodeShellSSH, self)._device_terminal_exit(
+		return super()._device_terminal_exit(
 			command=command
 		)
 
 	def _enable_paging(self):
-		"""Not in use"""
+		"""Not supported"""
 		pass
+
+	def strip_command(self, command_string: str, output: str) -> str:
+		"""
+		Strip command_string from output string
+
+		Cisco IOS adds backspaces into output for long commands (i.e. for commands that line wrap)
+
+		:param command_string: The command string sent to the device
+		:type command_string: str
+
+		:param output: The returned output as a result of the command string sent to the device
+		:type output: str
+		"""
+		# Support for Audiocode_Shell.
+		pattern= r'^SIP.*[\s\S]?PING.*>?.*[\s\S]?.*>?$'
+		output = re.sub(pattern, "", output, flags=re.M)
+
+		cmd=command_string.strip()
+		pattern = fr'{cmd}'
+		output = re.sub(pattern, "", output, flags=re.M)
+
+		return super().strip_command(
+			command_string=command_string,output=output
+		)
+
+	def strip_prompt(self, a_string: str) -> str:
+		"""Strip the trailing router prompt from the output.
+
+		:param a_string: Returned string from device
+		:type a_string: str
+		"""
+		pattern= r'^/>?'
+		a_string = re.sub(pattern, "", a_string, flags=re.M)
+		
+		return super().strip_prompt(
+			a_string=a_string,
+		)
 	
 class AudiocodeShellTelnet(AudiocodeShellSSH):
 	"""Audiocode this applies to 6.6 Audiocode Firmware versions that only use the Shell."""
